7e1ee8707ffd2f653e04865ac26563529f81c574
jaiman25400 <jaimanjoshi25@gmail.com>
2023-06-14 13:06:33 -0400

    Login/Signup  and Forget Password using Firebase

--- redux/store.js
+++ redux/store.js
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 
 import counterReducer from "./reducers/counterReducer";
+import userStateReducer from "./reducers/userStateReducer";
 
 const rootReducer = combineReducers({
   counter: counterReducer,
+  user: userStateReducer,
   //slice2: reducer2,
 });
 
